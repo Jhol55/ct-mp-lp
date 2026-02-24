5104730cb5e28678df6e7ebc4c02c94a72a70639
Jhonathan <jhonathan_galhardo@hotmail.com>
2026-02-24 19:22:51 -0300

    fix adminguard

--- src/partners/partners.module.js
+++ src/partners/partners.module.js
@@ -3,8 +3,10 @@ import { Module } from '@nestjs/common';
 import { PartnersController } from './partners.controller';
 import { PartnersService } from './partners.service';
 import { AdminGuard } from '../common/guards/admin.guard';
+import { AuthModule } from '../auth/auth.module.js';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PartnersController],
   providers: [PartnersService, AdminGuard],
 })
